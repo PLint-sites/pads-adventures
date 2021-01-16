d190ed13239c2eaa8715a09269782716943a2c4f
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-01-16 13:56:18 +0100

    Show image on listpage if available

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app dark>
+    <!--
     <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
       <v-list>
         <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
@@ -12,8 +13,9 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    -->
     <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-btn color="primary" nuxt :to="`/`">Home</v-btn>
@@ -23,16 +25,6 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>mdi-repeat</v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <v-col cols="12" sm="8" md="6">
       <v-card v-for="(article, index) of articles" :key="index" style="margin-bottom: 30px;">
         <v-card-title class="headline">{{article.title}}</v-card-title>
-        <v-card-text>{{article.description}}</v-card-text>
+        <v-card-text>
+          {{article.description}}
+          <img v-if="article.afbeelding_voor_listpage" :src="`${article.afbeelding_voor_listpage}`" alt="">
+        </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt :to="`/${article.slug}`">{{article.title}}</v-btn>
@@ -23,6 +26,7 @@ export default {
         error({ statusCode: 404, message: 'Article not found' })
       })
     store.commit('setTocFromArticles', articles)
+    console.log(articles)
     return {
       articles,
     }
