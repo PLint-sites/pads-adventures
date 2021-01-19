e1ee85b63c96ecb782252553a501eaba6e9b7705
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-01-19 14:52:50 +0100

    Fix title template in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - simple-cms',
+    titleTemplate: '%s - Pads adventures',
     title: 'Pads adventures',
     meta: [
       { charset: 'utf-8' },
