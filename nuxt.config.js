0f08c2f604a9f9ec8f19641019f752f46af91f2e
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-01-19 14:39:53 +0100

    Update title tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     titleTemplate: '%s - simple-cms',
-    title: 'simple-cms',
+    title: 'Pads adventures',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
