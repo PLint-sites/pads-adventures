ccb4db8d9c0426d7abbd04b9dbc7610c67e0def4
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-01-27 20:25:10 +0100

    Switch back changes made by accident

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - Writings',
+    titleTemplate: '%s - Avonturen van Pad',
     title: 'Inhoudsopgave',
     meta: [
       { charset: 'utf-8' },
