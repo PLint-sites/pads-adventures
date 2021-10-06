b4fcf599c0b535c68eab8bf0e20b40c8e2f0ceee
Pim Hooghiemstra <p.b.hooghiemstra@gmail.com>
2021-10-06 11:22:36 +0200

    Add back link to playground

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,8 @@
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn v-if="!isHome" color="primary" nuxt :to="`/`">Home</v-btn>
+      <v-btn v-if="!isHome" class="mr-3" color="primary" nuxt :to="`/`">Home</v-btn>
+      <a v-if="desktopSize" href="https://plint-demos.nl" title="Terug naar PLint's Playground">PLint's Playground</a>
     </v-app-bar>
     <v-main>
       <v-container>
@@ -12,6 +13,8 @@
     </v-main>
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
+      <v-spacer />
+      <a v-if="!desktopSize" href="https://plint-demos.nl" title="Terug naar PLint's Playground">PLint's Playground</a>
     </v-footer>
   </v-app>
 </template>
@@ -28,6 +31,7 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'Avonturen van Pad',
+      desktopSize: false,
       isHome: this.$route.path === '/'
     }
   },
@@ -44,6 +48,14 @@ export default {
         this.isHome = false
       }
     }
-  }
+  },
+  mounted() {
+    const mediaQuery = window.matchMedia('(max-width: 620px)')
+    if (mediaQuery.matches) {
+      this.desktopSize = false
+    } else {
+      this.desktopSize = true
+    }
+  },
 }
 </script>
